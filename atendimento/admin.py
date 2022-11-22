6fcb193d780705e881b3793f1d242a15fa43070d
Aluno <Aluno@politecnico.local>
2022-11-22 16:59:55 -0300

    AgendaCorrigido

---
+++ atendimento/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Atendimento
+
+
+@admin.register(Atendimento)
+class AtendimentoAdmin(admin.ModelAdmin):
+    list_display = ['horario','cliente','funcionario','servico','situacao']
+    search_fields = ('cliente', 'funcionario')
+    list_filter = ('horario', 'servico','situacao',)
+# Register your models here.
---
+++ produto/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from produto.models import Produto
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'fornecedor', 'quantidade')
+    search_fields = ('nome',)
+    list_filter = ('fornecedor',)
