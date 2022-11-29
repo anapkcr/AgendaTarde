d01eb84bc0f61f1bb642ce95d1c0f402b51563ba
Aluno <Aluno@politecnico.local>
2022-11-29 16:53:07 -0300

    funcionarios template

--- funcionario/views.py
+++ funcionario/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Funcionario
+
+class FuncionariosView(ListView):
+    model=Funcionario
+    template_name = 'funcionarios.html'
+
+    def get_queryset(self, *args, **kwargs):
+        buscar = self.request.GET.get('buscar')
+        qs = super(FuncionariosView, self).get_queryset(*args, **kwargs)
+        if buscar:
+            qs = qs.filter(nome__icontains=buscar)
+        return qs
 
 # Create your views here.
